ba4a162b4249c8bb09ca6617a75364ec0e0bb552
MingGo123 <834799422@qq.com>
2020-06-09 08:21:25 +0800

    first commit

---
+++ src/main/java/com/min/Hello.java
@@ -0,0 +1,17 @@
+package com.min;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Min
+ * @date 2020/6/9 07: 56 : 21
+ * description
+ */
+public class Hello {
+        public static void  main(String[] args)
+        {
+            System.out.println("你好");
+
+        }
+}
